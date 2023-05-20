dac2d2b061a4b5dc97667e8842ac45dfd6fa5651
Manav Gupta <37526340+manav98@users.noreply.github.com>
2023-05-20 18:07:16 +0530

    Hello world commit

---
+++ src/main/java/com/manav/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,19 @@
+package com.manav.springboot.learnspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retreiveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "Manav"),
+                new Course(2, "Learn Java", "Manav")
+        );
+    }
+}
